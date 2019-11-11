b99f69e4254ea7081c8f35949078c3013df3430d
vidura-supun <vidurasupun@gmail.com>
2019-11-11 13:55:03 +0530

    one more definition

--- guitar.py
+++ guitar.py
@@ -4,6 +4,7 @@ dlist =[]
 list1 = ['A', 'D', 'E']
 list2 = ['A', 'D', 'E', 'Am', 'Dm', 'Em']
 list3 = ['A', 'D', 'E', 'C', 'G', 'Am', 'Dm', 'Em']
+listn
 
 def main():
     banner = """
@@ -37,14 +38,15 @@ def main():
     print(banner)
     print("\n\n~Select the level~\n\n1.Beginner: A, D, E\n2.Not so beginner: A, D, E, Am, Dm, Em\n2. Intermediate: A, D, E, C, G, Am, Dm, Em\n\n")
     dec = input("LEVEL:")
-    if dec=="1": list == list1
-    elif dec=="2": list == list2
-    elif dec=="3": list == list3
+    if dec=="1": listn == list1
+    elif dec=="2": listn == list2
+    elif dec=="3": listn == list3
     else: defList()
 
 def defList():
-    print("Put in the chords you wanna practice CAWBOI-")
+    print("Put in the chords you wanna practice SPACE-CAWBOI-")
     custC = "1"
     while custC!="":
         custC = input("")
         dList.append(custC)
+    listn = dlist
